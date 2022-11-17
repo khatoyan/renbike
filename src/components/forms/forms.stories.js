b7ad7cb0935f76bffd018d7c545e3fd0e75a4d00
khatoyan <khatarm18@gmail.com>
2022-11-17 21:13:13 +0700

    form components added

---
+++ src/components/forms/forms.stories.js
@@ -0,0 +1,11 @@
+import loginForm from "./login_form.html"
+import cardForm from "./card_form.html"
+import registrationForm from "./registration_form.html"
+
+export default {
+    title: "компоненты/Form",
+};
+
+export const form  = () => loginForm;
+export const cardform  = () => cardForm;
+export const regForm = () => registrationForm;
