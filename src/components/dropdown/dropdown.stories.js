c23c8862458cc02e6efac7e09130e28b1a634d22
khatoyan <khatarm18@gmail.com>
2022-11-22 03:17:43 +0700

    feat: add header

---
+++ src/components/dropdown/dropdown.stories.js
@@ -0,0 +1,7 @@
+import dropdown from "./dropdown.html"
+
+export default {
+    title: "компоненты/dropdown",
+};
+
+export const Dropdown  = () => dropdown;
---
+++ src/components/header/header.stories.js
@@ -0,0 +1,9 @@
+import header from "./header.html"
+import headerLog from "./login-header.html"
+
+export default {
+    title: "компоненты/header",
+};
+
+export const Header  = () => header;
+export const HeaderLog  = () => headerLog;
